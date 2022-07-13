4945f9e1267383956e121ec507d514a40d2832ec
itsjuhee <itsjuheee@gmail.com>
2022-07-13 09:34:52 +0900

    first

---
+++ common.js
@@ -0,0 +1,13 @@
+var swiper1 = new Swiper(".swiper1", {
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+});
+
+var swiper2 = new Swiper(".swiper2", {
+scrollbar: {
+    el: ".swiper-scrollbar",
+    // hide: true,
+},
+});
